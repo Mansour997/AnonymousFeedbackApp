08139ceaffa6309cfff11896d0f20c1784f8dcb7
mansour997 <mansour.naji97@gmail.com>
2024-02-09 23:47:15 +0300

    Added the files once again

---
+++ src/test/java/com/example/AnonymousFeedbackApp/AnonymousFeedbackAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AnonymousFeedbackApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonymousFeedbackAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
